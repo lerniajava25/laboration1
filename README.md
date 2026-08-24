220e43d4ea09e14e836869d4a131496e8d091929
maju0029 <maju0029@gmail.com>
2026-08-24 15:17:07 +0200

    lagt till Jackson och api-klient för elpriser

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # laboration1
+
+"I projektets README.md fil, skriv en kort beskrivning av implementationen och hur du upplever att lösa uppgiften
+med Java jämfört med andra språk du programmerat i."
+
+Implimentation:
+Försöker tänka objektorienterat med uppdelning av klassobjekt. Tre packages, en för menu, en för api och en för metoder.
+
+Använder mig av en Switch/case menu. Arrow function i Java var nytt för mig. Läst lite Java tidigare och tror inte de fanns då. Fick då skriva ut allting!
+Detta liknar hur det fungerar i t.ex. JavaScript.
+
+API:
+Det var ganska svårt att förstå hur allt hänger ihop men tror jag förstått tillslut. (jämnföra med hur det var att skriva api i js.)
+
+metoder:
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,23 @@
 package org.example;
+import java.util.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
-        IO.println(String.format("This is a signed test commit"));
+        Scanner scanner = new Scanner(System.in);
 
+        IO.println("Välj nå!");
+        boolean active = true;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+        while (active) {
+            int val = scanner.nextInt();
+
+            switch (val) {
+                case 1 -> IO.println("Hello world!");
+                case 2 -> IO.println("Hello Pite!");
+                case 3 -> IO.println("Hello world!");
+                case 4 -> active = false;
+                default -> IO.println("Ogiltigt val");
+            };
         }
     }
 }
